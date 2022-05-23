b9bd8ff1ee11cb45defda3d8fcf44c2cc461d766
seif0912 <baddredineseif@gmail.com>
2022-05-23 20:48:40 +0100

    homepage: testimonial part 1

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ window.addEventListener('scroll', ()=>{
     let header = document.querySelector('header')
     header.classList.toggle('sticky', window.scrollY > 100)
 })
+
+let slideers = document.querySelectorAll('.testimonial .slider i')
+let boxes = document.querySelector('.testimonial .slider .testi')
+
